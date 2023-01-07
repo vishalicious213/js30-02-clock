59ecb88fe23f10a8d8cdf6e72954ebde6178d96a
vishalicious213 <vishalicious213@gmail.com>
2023-01-07 11:38:58 -0800

    grabbed the minutes hand and rotate it by the mins value in degrees

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 function setDate() {
     const secondHand = document.querySelector(".second-hand")
+    const minsHand = document.querySelector(".min-hand")
     const now = new Date()
-    const seconds = now.getSeconds()
-    // seconds/60 gives us the fraction or percentage out of 60
-    // multipilying it by 360 gives us the number out of the whole (which is the angle, in this case)
-    // adding 90 offsets our having rotated the .hand divs by 90 degrees to go from right-to-left to up-down
-    const secondsDegrees = (seconds / 60) * 360 + 90
 
+    const seconds = now.getSeconds()
+    const secondsDegrees = (seconds / 60) * 360 + 90
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-    console.log(seconds)
+
+    const mins = now.getMinutes()
+    const minsDegrees = (mins / 60) * 360 + 90
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`
+
+    // console.log(seconds)
 }
 
 setInterval(setDate, 1000);
+
+// seconds/60 gives us the fraction or percentage out of 60
+// multipilying it by 360 gives us the number out of the whole (which is the angle, in this case)
+// adding 90 offsets our having rotated the .hand divs by 90 degrees to go from right-to-left to up-down
