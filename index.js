26b73326989cb1b91c2ce763b5fe5eb52f227348
vishalicious213 <vishalicious213@gmail.com>
2023-01-07 11:33:06 -0800

    grabbed the seconds hand and rotate it by the seconds value in degrees

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function setDate() {
+    const secondHand = document.querySelector(".second-hand")
     const now = new Date()
     const seconds = now.getSeconds()
+    // seconds/60 gives us the fraction or percentage out of 60
+    // multipilying it by 360 gives us the number out of the whole (which is the angle, in this case)
+    // adding 90 offsets our having rotated the .hand divs by 90 degrees to go from right-to-left to up-down
+    const secondsDegrees = (seconds / 60) * 360 + 90
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
     console.log(seconds)
 }
 
